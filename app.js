f3ddce6863a841eae9cd0b409890c7f943f32029
Selahattin Ünlü <selahattin.unlu@yandex.com>
2021-07-24 15:25:56 +0300

    feat: scroll to contact us section when user click to 'get in touch' button

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 window.onload = () => {
-  const navigationLinks = document.querySelectorAll("header nav a");
+  const navigationLinks = document.querySelectorAll(
+    "[data-role='navigation-link']"
+  );
 
   navigationLinks.forEach((navigationLink) => {
     navigationLink.addEventListener("click", (e) => {
       e.preventDefault();
-      const targetId = navigationLink.getAttribute("href").replace("#", "");
+      const targetId = navigationLink
+        .getAttribute("data-href")
+        .replace("#", "");
       document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
     });
   });
