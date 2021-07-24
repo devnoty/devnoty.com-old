3ba51d7a759d5484c53727863eac7cde7627bde5
Selahattin Ünlü <selahattin.unlu@yandex.com>
2021-07-24 17:45:13 +0300

    feat: netlify-form integration for contact form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+  // navigation link
   const navigationLinks = document.querySelectorAll(
     "[data-role='navigation-link']"
   );
@@ -11,5 +12,51 @@ window.onload = () => {
         .replace("#", "");
       document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
     });
+  });
+
+  // contact form
+  const contactForm = document.querySelector('form[name="contact"]');
+  const successMessageEl = document.getElementById(
+    "contact-form-success-message"
+  );
+  const errorMessageEl = document.getElementById("contact-form-error-message");
+  const formSubmitButton = contactForm.querySelector("button");
+
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    successMessageEl.style.display = "none";
+    errorMessageEl.style.display = "none";
+    formSubmitButton.setAttribute("disabled", "true");
+
+    const formData = new FormData(contactForm);
+
+    if (
+      !formData.get("name") ||
+      !formData.get("email") ||
+      !formData.get("message")
+    ) {
+      errorMessageEl.style.display = "block";
+      errorMessageEl.textContent = "Please fill the form inputs";
+      formSubmitButton.removeAttribute("disabled");
+      return;
+    }
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => {
+        contactForm.querySelector("input[name='name']").value = "";
+        contactForm.querySelector("input[name='email']").value = "";
+        contactForm.querySelector("textarea").value = "";
+        successMessageEl.style.display = "block";
+        formSubmitButton.removeAttribute("disabled");
+      })
+      .catch((error) => {
+        formSubmitButton.removeAttribute("disabled");
+        errorMessageEl.style.display = "block";
+        errorMessageEl.textContent = error.message;
+      });
   });
 };
